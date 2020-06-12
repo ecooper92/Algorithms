cc52c2ed6969e8c4f9994d2d4208c74f1e398eb4
Eric Cooper <e.cooper92@gmail.com>
2020-06-12 11:34:02 -0500

    Made BinaryHeap a template

--- Algorithms/BinaryHeap.h
+++ Algorithms/BinaryHeap.h
@@ -3,16 +3,80 @@
 
 namespace Algorithms
 {
+	template <typename T>
 	class BinaryHeap
 	{
 	public:
-		BinaryHeap();
-		BinaryHeap(List<int> list);
-		void Insert(int item);
-		int Remove();
+		BinaryHeap() {}
+		BinaryHeap(List<T> list) :
+			_tree(list)
+		{
+			for (int i = _tree.Count() - 1; i >= 0; i--)
+			{
+				Heapify(i);
+			}
+		}
+
+		void Insert(T item)
+		{
+			_tree.Add(item);
+
+			int index = _tree.Count() - 1;
+			int lastIndex = -1;
+			while (index != lastIndex)
+			{
+				lastIndex = index;
+				index = (index - 1) / 2;
+				Heapify(index);
+			}
+		}
+
+		int Remove()
+		{
+			if (_tree.Count() == 1)
+			{
+				return _tree.Remove();
+			}
+			else
+			{
+				int first = _tree.First();
+				_tree[0] = _tree.Remove();
+
+				Heapify(0);
+
+				return first;
+			}
+		}
 	private:
-		void Heapify(int index);
-		List<int> _tree;
+		void Heapify(int index)
+		{
+			if (index < 0 || index >= _tree.Count())
+			{
+				return;
+			}
+
+			int smallestIndex = index;
+
+			int leftChildIndex = (index * 2) + 1;
+			if (leftChildIndex < _tree.Count() && _tree[leftChildIndex] < _tree[smallestIndex])
+			{
+				smallestIndex = leftChildIndex;
+			}
+
+			int rightChildIndex = leftChildIndex + 1;
+			if (rightChildIndex < _tree.Count() && _tree[rightChildIndex] < _tree[smallestIndex])
+			{
+				smallestIndex = rightChildIndex;
+			}
+
+			if (smallestIndex != index)
+			{
+				std::swap(_tree[smallestIndex], _tree[index]);
+				this->Heapify(smallestIndex);
+			}
+		}
+
+		List<T> _tree;
 	};
 }
 
